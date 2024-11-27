5ab66ceca364bc16d89cd4ff78de1b7d107f65bb
harrisonam <shrewdest.man86@gmail.com>
2024-11-27 11:49:33 -0800

    feat: add fund_app Django application configuration

---
+++ fund_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class FundAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fund_app'
+    verbose_name = "Faucet Application"
